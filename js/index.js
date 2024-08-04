b7a3ef439ea1072e8163b55baea87dd27aafe11e
Alice Izabeli <ereyzeel.nathr@gmail.com>
2024-08-03 21:50:05 -0300

    starting funcion filterbar

--- js/index.js
+++ js/index.js
@@ -1,4 +1,41 @@
+$(function(){
 
+    //Barra de progresso ta no 0
+    var currentValue = 0;
+    var isDrag = false;
+
+
+    //mouse pressionado = drag ativado
+    $('.pointerbar').mousedown(function(){
+        isDrag = true
+    })
+
+    
+    //mouse "inativo" = drag não
+    $(document).mouseup(function(){
+        isDrag = false
+    })
+
+
+    $('.pricebar').mousemove(function (e) { 
+        if(isDrag){
+
+            var elBase = $(this);
+            var mouseX = e.pageX - elBase.offset().left ;
+            if(mouseX < 0)
+                mouseX = 0
+            if(mouseX > elBase.width())
+                mouseX = elBase.width()
+        
+            var percent = (mouseX / elBase.width()) * 100;
+
+            console.log(percent);
+            
+        }
+        
+    });
+
+})
    
 
 
